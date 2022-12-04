279697f93d87d8be968c851d56e056e66fc14126
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2022-12-04 15:37:55 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4.3.8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp4._3._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввидите своё имя");
+            var name = Console.ReadLine();
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                Console.Write(name[i] + "");
+            }
+                
+            Console.ReadKey();
+        }
+    }
+}
